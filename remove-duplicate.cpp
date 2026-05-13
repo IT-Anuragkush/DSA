37d1b3e72b80f964ac1552f30cc633a66c3a6187
Anurag <anuragkushwaha708969@gmail.com>
2026-05-14 01:15:34 +0530

    new commit

---
+++ remove-duplicate.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void removeduplicate(vector<int> &arr)
+{
+  int k = 1;
+  for (int i = 1; i < arr.size(); i++)
+  {
+    if (arr[i] != arr[k - 1])
+    {
+      swap(arr[k], arr[i]);
+      // arr[k] = arr[i];
+      k++;
+    }
+  }
+  // return k;
+}
+int main()
+{
+  vector<int> arr = {0, 0, 1, 1, 2, 2, 2, 3, 4, 4, 5, 8};
+
+  removeduplicate(arr);
+
+  for (int i = 0; i < arr.size(); i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
--- remove-element.cpp
+++ remove-element.cpp
@@ -14,17 +14,18 @@ int removeElement(vector<int> &nums, int val)
       k++;
     }
   }
+
   return k;
 }
 
 int main()
 {
-  vector<int> nums = {5,8,9,8,3,8};
-  int val = 8;  
+  vector<int> nums = {5, 8, 9, 8, 3, 8};
+  int val = 8;
 
   int result = removeElement(nums, val);
 
-  for(int i = 0; i < result; i++)
+  for (int i = 0; i < result; i++)
   {
     cout << nums[i] << " ";
   }
